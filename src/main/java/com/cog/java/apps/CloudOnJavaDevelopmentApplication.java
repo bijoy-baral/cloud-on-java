893a380fd56ad042b9f209f55be425c5a937a7ba
Bijoy Baral <bijoy.on.java@gmail.com>
2019-06-27 20:02:42 +1000

    Initial Commit : Cloud App - Spring Boot Application

---
+++ src/main/java/com/cog/java/apps/CloudOnJavaDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.cog.java.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudOnJavaDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudOnJavaDevelopmentApplication.class, args);
+	}
+
+}
